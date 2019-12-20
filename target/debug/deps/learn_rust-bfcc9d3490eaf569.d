6ee3a81fc74f5cd0cb014641fdb789c368d621ca
alfinsuryaS <alfins132@gmail.com>
2019-12-20 18:08:57 +0800

    Remove target dir

--- target/debug/deps/learn_rust-bfcc9d3490eaf569.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Data/My Project/RUST/learn_rust/target/debug/deps/learn_rust-bfcc9d3490eaf569.rmeta: src/main.rs
-
-/Volumes/Data/My Project/RUST/learn_rust/target/debug/deps/learn_rust-bfcc9d3490eaf569.d: src/main.rs
-
-src/main.rs:
--- target/debug/learn_rust-9f00aa3d60cea73a.d
+++
@@ -1,1 +0,0 @@
-/Volumes/Data/My\ Project/RUST/learn_rust/target/debug/learn_rust-9f00aa3d60cea73a: /Volumes/Data/My\ Project/RUST/learn_rust/src/main.rs
